578b153d354869553c88ab10a4fdf7ec027737ae
Carlos Avila <carlosavila.ce@gmail.com>
2012-10-30 09:49:05 -0700

    initial setup

---
+++ jam/require.config.js
@@ -0,0 +1,37 @@
+var jam = {
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "chai",
+            "location": "jam/chai",
+            "main": "chai/chai.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "underscore",
+            "location": "jam/underscore",
+            "main": "underscore.js"
+        }
+    ],
+    "version": "0.2.11",
+    "shim": {}
+};
+
+if (typeof require !== "undefined" && require.config) {
+    require.config({packages: jam.packages, shim: jam.shim});
+}
+else {
+    var require = {packages: jam.packages, shim: jam.shim};
+}
+
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = jam;
+}
