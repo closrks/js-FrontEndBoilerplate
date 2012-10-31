10974d15de9be14239df82edbd7746dc32e8a9e2
Carlos Avila <carlosavila.ce@gmail.com>
2012-10-31 13:38:40 -0700

    Fully functional front template running sample test

---
+++ js/tests/sample.test.js
@@ -0,0 +1,10 @@
+var should = chai.should();
+
+describe('Array', function() {
+	describe('#indexOf()', function() {
+		it('should return -1 when the value is not present', function() {
+			var result = [1,2,3].indexOf(5);
+			result.should.equal(-1);
+		});
+	});
+});
